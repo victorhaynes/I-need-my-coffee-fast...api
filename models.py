d988a1d24776c6181b721b61c9eab51d77af0906
Victor Haynes <haynesvic@gmail.com>
2023-01-14 17:50:35 -0600

    Base models (coffee, rosater) initialized...installed psycopg2 and alembic

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+
+Base = declarative_base()
+
+
+class Coffee(Base):
+    __tablename__ = "coffees"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    roast = Column(String)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    roaster_id = Column(Integer, ForeignKey("roaster.id"))
+
+    roaster = relationship("Roaster")
+
+
+class Roaster(Base):
+    __tablename__ = "roasters"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
