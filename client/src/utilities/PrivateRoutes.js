0c948ecddf462814daf7eb17b341ad0fec3ce390
Victor Haynes <haynesvic@gmail.com>
2023-01-23 00:42:28 -0500

    fixed redirect logic, can now refresh on private route & still get redirected. Can als orefresh when logged in and stay logged in/not redirect

--- client/src/utilities/PrivateRoutes.js
+++ client/src/utilities/PrivateRoutes.js
@@ -1,18 +1,24 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 
 function PrivateRoutes({currentUser}) {
 
     const [loading, setLoading] = useState(true)
+    const [errors, setErrors] = useState(false)
+    const navigate = useNavigate()
 
     useEffect(() => {
         axios.get("/me").then(res => {
         setLoading(false)
-      })},[])
+        setErrors(false)
+      }).catch((err) => setErrors(err.response.data.detail.map((e) => e.msg)))}
+      ,[])
+
 
     return (
         <>
+            {errors ? <>{navigate("/login")}</> : <></>}
             {loading ? <></> : (currentUser ? <Outlet/> : <Navigate to="/login"/>)}
         </>
     )
